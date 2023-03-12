0970ad9a3144077770408f170ca3a4088bc5ca12
Li <varlamova_e.t@mail.ru>
2023-03-12 16:23:56 +0300

    fdfj

--- Reder.java
+++ Reder.java
@@ -1,9 +1,23 @@
 public class Reder{
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+
     public void showInfo(Object o) {
         if (o instanceof HasHealthPoint hasHealthPoint) {
             //String color = Color.GREEN.getColor();
             //System.out.println(color);
-            System.out.println(hasHealthPoint.percentHealth());
+            double healthPercent = hasHealthPoint.percentHealth();
+            if (healthPercent < 24.0) {
+                System.out.println(ANSI_BLACK + healthPercent + ANSI_BLACK);
+            } else if (healthPercent > 24.0 && healthPercent < 51.0) {
+                System.out.println(ANSI_RED + healthPercent + ANSI_RED);
+            } else if (healthPercent > 50 && healthPercent < 101) {
+                System.out.println(ANSI_GREEN + healthPercent + ANSI_GREEN);
+            }
+
             // Color c = Color.calculate(per);
             // System.out.println(c);
         }
